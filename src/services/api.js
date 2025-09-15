52f600aa7f13be48b07fb7674f5981028bcd3ca4
arshdhillon8560 <arshdhillon8560@gmail.com>
2025-09-15 12:51:10 +0530

    commit apiii

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://fin-tech-server.vercel.app/api',
+  baseURL: 'https://fin-tech-celebal-hackathon-bohl.vercel.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
