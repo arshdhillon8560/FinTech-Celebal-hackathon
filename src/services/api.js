c035e3e65df3da3e71a2830e215e3c465e8f25f1
arshdhillon8560 <arshdhillon8560@gmail.com>
2025-09-15 12:35:34 +0530

    new commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://fin-tech-server.vercel.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
